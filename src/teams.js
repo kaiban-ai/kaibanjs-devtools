8e870cdb4b61c91bc4ead476cd46c6dd991cab20
Dariel <dariel@front10.com>
2024-09-20 16:05:00 -0400

    Initial files... this is just an experiment.

---
+++ src/teams.js
@@ -0,0 +1,14 @@
+
+    // Auto-generated file
+    import team1 from '/Users/dariel/repositories/KaibanJS/agents-teams/bloggingTeam.kban.js';
+import team2 from '/Users/dariel/repositories/KaibanJS/bloggingTeam.kban.js';
+import team3 from '/Users/dariel/repositories/KaibanJS/resumeTeam.kban.js';
+
+    const teams = [
+      team1,
+      team2,
+      team3
+    ];
+
+    export default teams;
+  
